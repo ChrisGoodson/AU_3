5b7161b87d1be660eefb11eca69e49ec154e6446
Chris <f.chrisgoodson@icloud.com>
2023-02-05 14:14:53 -0600

    added label and refresh button, working on connecting them in the view controller

--- AU_3/ViewController.swift
+++ AU_3/ViewController.swift
@@ -8,11 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func refreshTapped(_ sender: UIButton) {
+        
+        let currentDateTime = Date()
+        let formatter = DateFormatter()
+        
+        formatter.timeStyle = .medium
+        formatter.dateStyle = .long
+        
+        let dateTimeString = formatter.string(from: currentDateTime)
+        timeLabel.text = "Current date and time: " + dateTimeString
+        
+    }
+    
+    
 
 
 }
