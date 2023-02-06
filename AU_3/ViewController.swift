277a2c997d2743cbe9f9e7779aecfff19565f4dc
Chris <f.chrisgoodson@icloud.com>
2023-02-05 19:34:32 -0600

    everything is working except the button press. that's still not working for some reason. it should tho

--- AU_3/ViewController.swift
+++ AU_3/ViewController.swift
@@ -4,40 +4,81 @@ import AVFoundation
 class ViewController: UIViewController {
     
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var countdownLabel: UILabel!
+    @IBOutlet weak var countdownTimer: UIDatePicker!
+    @IBOutlet weak var startButton: UIButton!
     
     var timer = Timer()
-    var countdownTimer = 60
+    var countdown = 0
     var chimeSound: AVAudioPlayer?
+    var isRunning = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        guard let url1 = Bundle.main.url(forResource: "ayntk", withExtension: "mp3") else { return }
+     
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] timer in
+                let currentDateTime = Date()
+                let formatter = DateFormatter()
+                formatter.timeStyle = .long
+                formatter.dateStyle = .short
+                let dateTimeString = formatter.string(from: currentDateTime)
+                self?.timeLabel.text = dateTimeString
+            }
+        
+        let currentDateTime = Date()
+        let formatter = DateFormatter()
 
+        formatter.timeStyle = .long
+        formatter.dateStyle = .short
+
+        let dateTimeString = formatter.string(from: currentDateTime)
+        timeLabel.text = dateTimeString
+
+        // Do any additional setup after loading the view.
+        countdownTimer.datePickerMode = .countDownTimer
+        countdownTimer.countDownDuration = 0
+        
+        guard let url1 = Bundle.main.url(forResource: "ayntk", withExtension: "mp3") else { return }
+        
         do {
             chimeSound = try AVAudioPlayer(contentsOf: url1)
-            chimeSound?.play()
         } catch let error {
             print("error \(error.localizedDescription)")
         }
     }
     
-    @IBAction func startButtonTapped(_ sender: Any) {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        if isRunning {
+            stopTimer()
+        } else {
+            startTimer()
+        }
     }
     
-    @IBAction func stopButtonTapped(_ sender: Any) {
+    func startTimer() {
+        isRunning = true
+        startButton.setTitle("Stop", for: .normal)
+        countdown = Int(countdownTimer.countDownDuration)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+
+    
+    func stopTimer() {
+        isRunning = false
+        startButton.setTitle("Start", for: .normal)
         timer.invalidate()
-        chimeSound?.stop()
+        countdownLabel.text = "Create a timer"
     }
     
     @objc func updateTime() {
-        if countdownTimer > 0 {
-            countdownTimer -= 1
-            secondLabel.text = "\(countdownTimer) seconds left"
+        let hours = Int(countdown / 3600)
+        let minutes = Int((countdown % 3600) / 60)
+        let seconds = Int(countdown % 60)
+        countdownLabel.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        
+        if countdown > 0 {
+            countdown -= 1
         } else {
-            timer.invalidate()
             chimeSound?.play()
         }
     }
